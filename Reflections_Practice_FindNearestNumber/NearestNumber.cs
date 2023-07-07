4ab50587c0d3543bda744579fc53e0cb260b5190
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-07 16:45:33 +0530

    [somu]Add:Finding Nearest Number.

---
+++ Reflections_Practice_FindNearestNumber/NearestNumber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflections_Practice_FindNearestNumber
+{
+    public class NearestNumber
+    {
+        public static void FindNearestNumber(int n)
+        {
+            int absnumber=Math.Abs(n);
+            int closestNumber = Int32.MaxValue;
+            int minDifference = Int32.MaxValue;
+           for(int i = absnumber; i <= absnumber+10; i++)
+            {
+                if(IsEvenNumber(i))
+                {
+                    int difference = Math.Abs(absnumber - i);
+                    if(difference < minDifference)
+                    {
+                        closestNumber = i;
+                        minDifference = difference;
+                    }
+                    else if(difference ==  minDifference && i < closestNumber)
+                    {
+                        closestNumber = i;
+                    }
+                }
+           }
+            Console.WriteLine(Math.Sign(n)* closestNumber);
+        }
+        public static bool IsEvenNumber(int number)
+        {
+            while(number != 0)
+            {
+                int digit=number % 10;
+                if(digit%2 !=0 )
+                {
+                    return false;
+                }
+                number/=10;
+            }
+            return true;
+        }
+    }
+}
---
+++ Reflections_Practice_FindNearestNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Reflections_Practice_FindNearestNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //NearestNumber.FindNearestNumber(-33);
+        }
+    }
+}
