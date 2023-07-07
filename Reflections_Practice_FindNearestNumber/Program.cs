78c22ecaf60b1b0955f9a49f8a5e7ffffcdbae25
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-07 17:10:36 +0530

    [somu]Add:Finding Nearest Number.

--- Reflections_Practice_FindNearestNumber/Program.cs
+++ Reflections_Practice_FindNearestNumber/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Reflections_Practice_FindNearestNumber
+﻿using System.Reflection;
+
+namespace Reflections_Practice_FindNearestNumber
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            //NearestNumber.FindNearestNumber(-33);
+            NearestNumber.FindNearestNumber(-33);           
         }
     }
 }
