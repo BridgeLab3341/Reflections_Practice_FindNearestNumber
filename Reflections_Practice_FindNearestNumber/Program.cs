237f45c4ff067c024b3127954ee40b9c7bf9244c
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-07 17:15:09 +0530

    [somu]Add:Fetching All Class Members(Methods,Constructor,Properties)Using Reflection.

--- Reflections_Practice_FindNearestNumber/Program.cs
+++ Reflections_Practice_FindNearestNumber/Program.cs
@@ -6,7 +6,31 @@ namespace Reflections_Practice_FindNearestNumber
     {
         public static void Main(string[] args)
         {
-            NearestNumber.FindNearestNumber(-33);           
+            NearestNumber.FindNearestNumber(-33);
+            Type type = typeof(NearestNumber);
+            Console.WriteLine("Fetching Methods");
+            Console.WriteLine("-----------------");
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.Name);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Fetching Constructors");
+            Console.WriteLine("----------------------");
+            ConstructorInfo[] constructors = type.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.Name);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Fetching Properties");
+            Console.WriteLine("---------------------");
+            PropertyInfo[] propertyInfos = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            foreach (PropertyInfo property in propertyInfos)
+            {
+                Console.WriteLine(property.Name);
+            }
         }
     }
 }
